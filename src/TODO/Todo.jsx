0b04fba5de873156a91f94e0a060c863f1448451
alamnahid <nahidalam13765@gmail.com>
2023-11-13 17:11:19 +0600

    Loading states should be shown in the UI (while fetching data)

--- src/TODO/Todo.jsx
+++ src/TODO/Todo.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
 import Card from "../Components/Card/Card";
+import Loading from "../Components/Loading/Loading";
 
 
 const Todo = () => {
     const [todoData, setTodoData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         try {
             fetch('https://jsonplaceholder.typicode.com/todos')
                 .then(res => res.json())
-                .then(data => setTodoData(data))
+                .then(data => {
+                    setTodoData(data)
+                    setLoading(false)
+                })
         }
         catch (error) {
             console.error('error')
         }
     }, [])
 
+    if(loading){
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-           {
-            todoData.map((card, index)=> <Card key={index} card={card}></Card>)
-           }
+            {
+                todoData.map((card, index) => <Card key={index} card={card}></Card>)
+            }
         </div>
     );
 };
