58bfb3ff659b73fd4aa0329afe65066772d6ab31
alamnahid <nahidalam13765@gmail.com>
2023-11-13 19:29:37 +0600

    data filter using dropdown

--- src/TODO/Todo.jsx
+++ src/TODO/Todo.jsx
@@ -6,6 +6,8 @@ import Loading from "../Components/Loading/Loading";
 const Todo = () => {
     const [todoData, setTodoData] = useState([])
     const [loading, setLoading] = useState(true)
+    const [status, setStatus] = useState('all');
+    const [showData, setShowData] = useState([])
 
     useEffect(() => {
         try {
@@ -15,21 +17,63 @@ const Todo = () => {
                     setTodoData(data)
                     setLoading(false)
                 })
+                .catch((error)=>console.log(error))
         }
         catch (error) {
             console.error('error')
         }
     }, [])
 
+  const handleFilter = (e)=>{
+    e.preventDefault()
+    const filter = e.target.value;
+    console.log(filter)
+    if(filter==='true'){
+      setStatus('true')
+    setShowData(todoData.filter(todo=>todo.completed===true))
+    }
+    if(filter==="false"){
+      setStatus('false')
+      setShowData(todoData.filter(todo=>todo.completed===false))
+    }
+    if(filter==="all"){
+      setStatus('all')
+      setShowData(todoData)
+    }
+
+  }
+
+  useEffect(()=>{
+    if(status==='all'){
+      setShowData(todoData)
+    }
+  }, [status, todoData])
+    
     if(loading){
         return <Loading></Loading>
     }
 
     return (
         <div>
+
+        <label>
+        Show Completed:
+        <select
+         name="filter"
+         onChange={handleFilter}
+         defaultValue={'all'}
+        >
+          <option value="all">All</option>
+          <option value="true">Complete</option>
+          <option value="false">Filter</option>
+        </select>
+      </label>
+
+            <div>
             {
-                todoData.map((card, index) => <Card key={index} card={card}></Card>)
+                showData.map((card, index) => <Card key={index} card={card}></Card>)
             }
+        </div>
         </div>
     );
 };
